01d450a48db6257736aec3ab7c1dd1c646f3f1b8
Kauê Gimenes <kaue@dot.net>
2016-06-02 20:09:59 +0000

    adicionar script.js

---
+++ public/js/script.js
@@ -0,0 +1,20 @@
+$('document').ready(function(){
+    $('.multi-item-carousel').carousel({
+        interval: false
+    });
+    
+    $('.multi-item-carousel .carousel-item').each(function() {
+        var next = $(this).next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+        next.children(':first-child').clone().appendTo($(this));
+    
+        if (next.next().length > 0) {
+            next.next().children(':first-child').clone().appendTo($(this));
+        }
+        else {
+            $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+        }
+    });    
+})
