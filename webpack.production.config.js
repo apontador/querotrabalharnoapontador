9fbfeba699f4b78f97e1fb312392044622b76ca8
Mauricio N. Reis <mauricioskateboard@gmail.com>
2016-06-07 18:20:16 -0300

    subindo

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -12,7 +12,7 @@ module.exports = {
     ],
     output: {
         path: path.join(__dirname, 'dist'),
-        filename: 'app.js'
+        filename: 'app.min.js'
     },
     module: {
         loaders: require('./loaders')
