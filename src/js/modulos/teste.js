f1c5182e4d8115425e1326893577009193d55815
remacedo <remacedo82@gmail.com>
2016-07-26 14:41:00 -0300

    Ajustes index

--- src/js/modulos/teste.js
+++ src/js/modulos/teste.js
@@ -4,28 +4,6 @@
 // // $scope.greeting = { text: 'Hello 1201545454545454' };
 
 
-// $http({
-//         url: 'https://api.apontador.com.br/v2/places/B37822W2/photos',
-//         method: 'GET',
-//         data: "token='9b68efc6-67c1-4d94-9be6-1dc8f20be665'"
-// });
-
-
-// // // $http.get('https://api.apontador.com.br/v2/oauth/token', {headers: {'client_id=testefront&secret_id=jO98nsGN6rXlmWXKKxnwS9NogdD~&grant_type=client_credentials'}})
-// // // .then(function(response) {
-// // //         console.log('response', response);
-        
-// // // });
-
-
-
-// // // $http.get('https://api.apontador.com.br/v2/oauth/authorize?client_id=testefront&redirect_uri=http://localhost:3000/build/&scope=read,write&response_type=code')
-// // // .then(function(response) {
-// // //         console.log('response', response);
-// // // });
-
-
-
 // //   $http.get('https://api.apontador.com.br/v2/places/B37822W2/photos', {
 // //     headers: {
 // //         "Authorization": 'Token token="code=VHLhrl"',
