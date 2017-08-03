6f6e2bb98599633f0a3c5c3c6f7ffd52ead6842b
gjlacerda <guilherme.lcd@gmail.com>
2017-08-03 19:53:43 -0300

    Configurado Webpack integrado com React e webpack-dev-server utilizando LiveReload

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path    = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    devtool: 'inline-source-map',
+    entry: [
+        'webpack-dev-server/client?http://localhost:8080',
+        'webpack/hot/dev-server',
+        './src/index.js'
+    ],
+    output: {
+        path: path.resolve('dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: ['babel-loader?presets[]=react,presets[]=es2015']
+            }
+        ]
+    },
+    resolve: {
+        extensions: [
+            '*',
+            '.js',
+            '.jsx'
+        ],
+        modules: [
+            'src',
+            'node_modules'
+        ]
+    },
+    devServer: {
+        publicPath: '/',
+        contentBase: './dist',
+        historyApiFallback: true
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoEmitOnErrorsPlugin()
+    ]
+};
