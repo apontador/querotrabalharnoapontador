0557be6a62bd4759d4833016b227f8811f7530ff
gjlacerda <guilherme.lcd@gmail.com>
2017-08-03 21:48:45 -0300

    Alterado fonte principal

--- src/styled/app/global.styled.js
+++ src/styled/app/global.styled.js
@@ -1,6 +1,11 @@
 import {injectGlobal} from 'styled-components';
 
 injectGlobal`
+
+    body {
+        font-family: "Helvetica Neue", Arial, Verdana;
+    }
+    
     * {
         box-sizing: border-box;
     }
