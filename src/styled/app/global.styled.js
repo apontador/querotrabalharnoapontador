3c8e7e263a7c4eca0ecae095467ff7b2a29921f3
gjlacerda <guilherme.lcd@gmail.com>
2017-08-04 00:02:50 -0300

    Adicionado fonte source sans pro e melhorias gerais nos estilos

--- src/styled/app/global.styled.js
+++ src/styled/app/global.styled.js
@@ -1,11 +1,22 @@
 import {injectGlobal} from 'styled-components';
+import {bodyBg, grayMain} from 'styled/app/colors.styled';
 
 injectGlobal`
 
+    @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400');
+
     body {
-        font-family: "Helvetica Neue", Arial, Verdana;
+        background-color: ${bodyBg};
+        color: ${grayMain};
+        font-family: 'Source Sans Pro', sans-serif;
+        font-size: 15px;
+        font-weight: 300;
     }
     
+    main {
+        margin-top: 20px;
+    }
+ 
     * {
         box-sizing: border-box;
     }
--- src/styled/app/row.styled.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const Row = styled.div`
-    max-width: 960px;
-    margin: 0 auto;
-    padding: 0 5px;
-`;
-
-export default Row;
