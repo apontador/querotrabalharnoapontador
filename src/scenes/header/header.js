39ac3b532d3fddb9be88108ec053a3e80d017859
gjlacerda <guilherme.lcd@gmail.com>
2017-08-03 21:49:01 -0300

    Criado header principal

---
+++ src/scenes/header/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Container, Content, Logo} from 'styled/scenes/header/header.styled';
+
+const Header = () => (
+    <Container>
+        <Content>
+            <Logo href="https://www.apontador.com.br/"/>
+        </Content>
+    </Container>
+);
+
+export default Header;
---
+++ src/styled/scenes/header/header.styled.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+import Row from 'styled/app/row.styled';
+
+const headerColor = '#ff8207';
+
+export const Container = styled.header`
+    height: 52px;
+    background-color: ${headerColor}
+`;
+
+export const Content = Row.extend`
+    display: flex;
+    align-items: center;
+    height: 100%;
+`;
+
+export const Logo = styled.a`
+    display: block;
+    width: 156px;
+    height: 43px;
+    background-image: url(assets/images/logo.png);    
+`;
