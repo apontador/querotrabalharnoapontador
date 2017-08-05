8342698c31a87761ddbf7b419755ed5789dc8fe0
gjlacerda <guilherme.lcd@gmail.com>
2017-08-05 14:38:57 -0300

    Helper para calcular o tamanho proporcional de uma imagem de acordo com o tamanho dela e o container

---
+++ src/helpers/proportionalSize.helper.js
@@ -0,0 +1,46 @@
+/**
+ * Calcula o tamanho proporcional de acordo com o tamanho da imagem e container
+ * @param originalWidth
+ * @param originalHeight
+ * @param maxWidth
+ * @param maxHeight
+ * @returns {*}
+ */
+const proportionalSize = (originalWidth, originalHeight, maxWidth, maxHeight) => {
+
+    let width, height;
+
+    if (originalWidth < maxWidth && originalHeight < maxHeight) {
+        return {
+            width: originalWidth,
+            height: originalHeight
+        };
+    }
+
+    // Paisagem (assumindo paisagem)
+    width  = (originalWidth > maxWidth) ? maxWidth : originalWidth;
+    height = Math.ceil((width * originalHeight) / originalWidth);
+
+    // Retrato
+    if (originalHeight > originalWidth) {
+        height = (originalHeight > maxHeight) ? maxHeight : originalHeight;
+        width  = Math.ceil((height * originalWidth) / originalHeight);
+    }
+
+    if (width > maxWidth) {
+        width  = maxWidth;
+        height = Math.ceil((width * originalHeight) / originalWidth);
+    }
+
+    if (height > maxHeight) {
+        height = maxHeight;
+        width  = Math.ceil((height * originalWidth) / originalHeight);
+    }
+
+    return {
+        width: width,
+        height: height
+    };
+};
+
+export default proportionalSize;
