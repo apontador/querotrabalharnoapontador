ea25a98100f4527c4b560e450e9c3514f7a4398d
gjlacerda <guilherme.lcd@gmail.com>
2017-08-05 18:40:53 -0300

    Adicionado cor cinza clara

--- src/styled/app/colors.styled.js
+++ src/styled/app/colors.styled.js
@@ -1,3 +1,4 @@
-export const bodyBg   = '#efeff4';
-export const grayMain = '#4e595d;';
+export const bodyBg    = '#efeff4';
+export const grayMain  = '#4e595d;';
+export const grayLight = '#999';
 export const mainColor = '#f3781f';
