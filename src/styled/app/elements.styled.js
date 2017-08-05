1e93c7883fc6c89b4f082bd77e1d002e488f3df2
gjlacerda <guilherme.lcd@gmail.com>
2017-08-05 20:02:46 -0300

    Melhorias gerais de estilo

--- src/styled/app/elements.styled.js
+++ src/styled/app/elements.styled.js
@@ -1,8 +1,14 @@
 import styled from 'styled-components';
+import {grayLight} from 'styled/app/colors.styled';
 
 export const H1 = styled.h1`
     margin-top: 0;
-    margin-bottom: 10px;
-    font-size: 20px;
+    margin-bottom: 8px;
+    font-size: 22px;
     font-weight: 400;
+`;
+
+export const P = styled.p`
+    margin: 0 0 4px 0;
+    color: ${grayLight};
 `;
