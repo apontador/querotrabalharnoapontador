a999cb8fa209f863ef263a4c02e7b5ac29819d18
gjlacerda <guilherme.lcd@gmail.com>
2017-08-05 20:02:38 -0300

    Passagem de dados para os componentes filhos

--- src/scenes/home/home.js
+++ src/scenes/home/home.js
@@ -1,15 +1,53 @@
 import React from 'react';
 import Info from 'components/info/info';
+import Venue from 'components/venue/venue';
+import ApontadorService from 'services/apontador.service';
 
 export default class Home extends React.Component {
+
+    constructor() {
+
+        super();
+
+        this.apontadorService = new ApontadorService();
+
+        this.state = {
+            place: {
+                address: {},
+                categories: [],
+                statistics: {},
+                phones: [],
+                openingHours: {}
+            }
+        };
+    }
+
+    componentWillMount() {
+        this.getInfo();
+    }
 
     render() {
 
         return (
             <main>
-                <Info/>
+                <Info place={this.state.place}/>
+                <Venue place={this.state.place}/>
                 <div id="modalContainer"></div>
             </main>
         );
+    }
+
+    /**
+     * Busca as informações do local
+     * @returns {*}
+     */
+    getInfo() {
+
+        return this.apontadorService.getPlaceInfo().then(response => {
+            console.log(response);
+            this.setState({
+                place: response.place
+            });
+        });
     }
 }
