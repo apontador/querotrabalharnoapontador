011f1877cf23d2cf9c4f09c73f0b149f037b5f39
gjlacerda <guilherme.lcd@gmail.com>
2017-08-05 20:57:33 -0300

    Ícones de votar

--- src/styled/components/statistics/statistics.styled.js
+++ src/styled/components/statistics/statistics.styled.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 const rates = {
-    5: '#8dbd70',
-    4: '#bedb96',
+    5: '#00B551',
+    4: '#8dbd70',
     3: '#ecde78',
     2: '#edbf6f',
     1: '#e2403b'
@@ -16,7 +16,6 @@ export const Container = styled.span`
     color: white;
     padding: 2px 8px;
     border-radius: 5px;
-    margin-left: 10px;
     font-size: 16px;
     font-weight: 300;
     background-color: ${props => getRateColor(props.rate)};
