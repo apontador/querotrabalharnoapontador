619e747d0ef81da41b2d7de2d35e26a2650cdfb6
gjlacerda <guilherme.lcd@gmail.com>
2017-08-06 18:41:54 -0300

    Criado componente fake de avaliações

---
+++ src/components/navigation/evaluation.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Container} from 'styled/components/navigation/evaluation.styled';
+
+const Evaluations = () => (
+    <Container id="evaluation">TODO: lista de avaliações aqui</Container>
+);
+
+export default Evaluations;
