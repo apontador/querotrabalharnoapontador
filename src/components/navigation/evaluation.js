0bcff819a54c48cab89febc659863806edc983fc
gjlacerda <guilherme.lcd@gmail.com>
2017-08-06 22:53:38 -0300

    Adicionado mensagem de explicação

--- src/components/navigation/evaluation.js
+++ src/components/navigation/evaluation.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Container} from 'styled/components/navigation/evaluation.styled';
 
 const Evaluations = () => (
-    <Container id="evaluation">TODO: lista de avaliações aqui</Container>
+    <Container id="evaluation">Criei esta seção só para ilustrar as abas</Container>
 );
 
 export default Evaluations;
